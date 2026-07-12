a57fde05bdc25aef47dfdba3484c3c6e77514a6d
prasanna sfc <prasannasfc58@gmail.com>
2026-07-12 23:32:30 +0530

    server port changes

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express")
 const cors = require("cors")
 
 const app = express()
-const port = process.env.PORT
+const port = process.env.PORT || 5000;
 app.use(express.json())
 app.use(express.urlencoded({extended:true}));
 app.use(cors({
@@ -38,6 +38,6 @@ app.get("/api/health-check", (req, res) => {
     });
 });
 
-app.listen(5000,'0.0.0.0',()=>{
-    console.log(`Server running on port ${5000}`,);
+app.listen(port,'0.0.0.0',()=>{
+    console.log(`Server running on port ${port}`,);
 })
